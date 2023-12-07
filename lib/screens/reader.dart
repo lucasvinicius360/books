5667c8919ce31bdf855d3ef12b3af3954fd663bc
Lucas Vinicius <volgfan@gmail.com>
2023-12-07 12:07:18 -0300

    leitura do livro

--- lib/screens/reader.dart
+++ lib/screens/reader.dart
@@ -1,23 +1,66 @@
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:dio/dio.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:vocsy_epub_viewer/epub_viewer.dart';
-import '../models/book.dart';
 
-class EpubViewerScreen extends StatefulWidget {
-  final Book book;
-
-  const EpubViewerScreen({Key? key, required this.book}) : super(key: key);
-
-  @override
-  _EpubViewerScreenState createState() => _EpubViewerScreenState();
+void main() async {
+  runApp(MyApp());
 }
 
-class _EpubViewerScreenState extends State<EpubViewerScreen> {
-  bool loading = false;
-  String filePath = "";
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool loading = false; // Flag para controlar se o download está em andamento
+  Dio dio = Dio(); // Objeto para realizar operações de download HTTP
+  String filePath = ""; // Caminho do arquivo EPUB baixado
+
+  @override
+  void initState() {
+    download(); // Método chamado ao iniciar o aplicativo para iniciar o download
+    super.initState();
+  }
+
+  // Lógica para verificar permissões e iniciar o download
+  download() async {
+    if (Platform.isAndroid || Platform.isIOS) {
+      String? firstPart;
+      final deviceInfoPlugin = DeviceInfoPlugin();
+      final deviceInfo = await deviceInfoPlugin.deviceInfo;
+      final allInfo = deviceInfo.data;
+
+      // Lógica para extrair a primeira parte da versão do sistema operacional
+      if (allInfo['version']["release"].toString().contains(".")) {
+        int indexOfFirstDot = allInfo['version']["release"].indexOf(".");
+        firstPart = allInfo['version']["release"].substring(0, indexOfFirstDot);
+      } else {
+        firstPart = allInfo['version']["release"];
+      }
+
+      int intValue = int.parse(firstPart!);
+
+      // Verificar a versão do sistema operacional para determinar se é necessário solicitar permissões
+      if (intValue >= 13) {
+        await startDownload();
+      } else {
+        if (await Permission.storage.isGranted) {
+          await Permission.storage.request();
+          await startDownload();
+        } else {
+          await startDownload();
+        }
+      }
+    } else {
+      loading =
+          false; // Se não for Android ou iOS, não há download em andamento
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +68,7 @@ class _EpubViewerScreenState extends State<EpubViewerScreen> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(widget.book.title),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+          title: const Text('Vocsy Plugin E-pub example'),
         ),
         body: Center(
           child: loading
@@ -51,7 +86,7 @@ class _EpubViewerScreenState extends State<EpubViewerScreen> {
                       onPressed: () async {
                         print("=====filePath======$filePath");
                         if (filePath == "") {
-                          // download(); // Iniciar o download se o arquivo EPUB não estiver baixado
+                          download(); // Iniciar o download se o arquivo EPUB não estiver baixado
                         } else {
                           // Configurações e abertura do EPUB online
                           VocsyEpub.setConfig(
@@ -123,29 +158,21 @@ class _EpubViewerScreenState extends State<EpubViewerScreen> {
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    startDownload();
-  }
-
+  // Iniciar o download do EPUB da internet
   // Iniciar o download do EPUB da internet
   startDownload() async {
     Directory? appDocDir = Platform.isAndroid
         ? await getExternalStorageDirectory()
         : await getApplicationDocumentsDirectory();
 
-    String path = appDocDir!.path + '/${widget.book.id}.epub';
+    String path = appDocDir!.path + '/sample.epub';
     File file = File(path);
 
-    debugPrintStack(label: '${widget.book.download_url}-------> test ');
-    
-
     // Se o arquivo EPUB não existir, crie-o e inicie o download
-    if (!file.existsSync()) {
+    if (!File(path).existsSync()) {
       await file.create();
-      await Dio().download(
-        "${widget.book.download_url}",
+      await dio.download(
+        "https://www.gutenberg.org/ebooks/72127.epub.images", // Substitua a URL aqui
         path,
         deleteOnError: true,
         onReceiveProgress: (receivedBytes, totalBytes) {
@@ -157,42 +184,46 @@ class _EpubViewerScreenState extends State<EpubViewerScreen> {
         setState(() {
           loading = false;
           filePath = path;
-          openEpub();
         });
       });
     } else {
       setState(() {
         loading = false;
         filePath = path;
-        openEpub();
       });
     }
   }
 
-  // Abrir o livro EPUB
-  void openEpub() {
-    if (filePath.isNotEmpty) {
-      VocsyEpub.open(filePath);
-    }
-  }
-}
+  // Iniciar o download do EPUB da internet
+  // startDownload() async {
+  //   Directory? appDocDir = Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
 
-class YourEpubViewerWidget extends StatelessWidget {
-  final String filePath;
+  //   String path = appDocDir!.path + '/sample.epub';
+  //   File file = File(path);
 
-  const YourEpubViewerWidget({Key? key, required this.filePath})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Text(
-          filePath, // Exibindo o caminho do arquivo (para depuração)
-          style: TextStyle(fontSize: 16.0),
-        ),
-      ),
-    );
-  }
+  //   // Se o arquivo EPUB não existir, crie-o e inicie o download
+  //   if (!File(path).existsSync()) {
+  //     await file.create();
+  //     await dio.download(
+  //       "https://www.gutenberg.org/ebooks/72134.epub3.images",
+  //       path,
+  //       deleteOnError: true,
+  //       onReceiveProgress: (receivedBytes, totalBytes) {
+  //         setState(() {
+  //           loading = true;
+  //         });
+  //       },
+  //     ).whenComplete(() {
+  //       setState(() {
+  //         loading = false;
+  //         filePath = path;
+  //       });
+  //     });
+  //   } else {
+  //     setState(() {
+  //       loading = false;
+  //       filePath = path;
+  //     });
+  //   }
+  // }
 }
